ba971ff98a584df1e029647bc08ee2bdd7b6e58e
Marfater Rahman Jabed <marfaterrahman@gmail.com>
2022-10-11 23:39:40 +0600

    home page header added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import './Home.css'
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <div className='home'>
+                <img src="https://t4.ftcdn.net/jpg/02/04/23/79/360_F_204237911_xyKrPNLFAWwmM7pm6t3OKpj1yuI0T0KH.webp" alt="" />
+                <h1 className='color'>TEST YOURSELF HERE</h1>
+            </div><br /><br />
+            <div>
+                <h1>herer are the topic</h1>
+            </div>
         </div>
     );
 };
