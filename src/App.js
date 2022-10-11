1f7bb58edc9c83b069c1ad35fa609b5e4a0caf6a
Marfater Rahman Jabed <marfaterrahman@gmail.com>
2022-10-11 17:31:10 +0600

    header item successfully added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import logo from './logo.svg';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './component/Blog/Blog';
 import NavBar from './component/NavBar/NavBar';
+import Statistics from './component/Statistics/Statistics';
+import Topic from './component/Topic/Topic';
+import Main from './layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+
+        {
+          path: '/topic',
+          element: <Topic></Topic>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
+    },
+
+  ])
   return (
-    <div>
-      <NavBar></NavBar>
+    <div >
+      {/* <NavBar></NavBar> */}
+      <RouterProvider router={router}></RouterProvider>
+
     </div>
   );
 }
--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>this is navBar</h1>
+        <div className='navbar'>
+            <NavLink className={({ isActive }) => isActive ? 'active' : undefined} to='/topic'>Topic</NavLink>
+            <NavLink to='/statistics'>Statistics</NavLink>
+            <NavLink to='/blog'>Blog</NavLink>
+
         </div>
     );
 };
